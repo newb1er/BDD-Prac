4ab8a773d20c0a69b18d98652bc1e360c7aa0457
newb1er <taiwanmichael429@gmail.com>
2022-03-28 02:25:11 +0800

    feature code implement Feature: selenium controll Scenario: Open NYCU home page

--- src/web_test.py
+++ src/web_test.py
@@ -9,12 +9,13 @@ class WebTestStep(ABC):
 
     def __init__(self, url):
         self.url = url
+        self.driver = None
 
-    def navigate(self, driver):
-        driver.get(self.url)
+    def navigate(self):
+        self.driver.get(self.url)
 
     @abstractmethod
-    def action(self, driver):
+    def action(self):
         return NotImplemented
 
 
@@ -24,12 +25,26 @@ class NYCUTestStep(WebTestStep):
         url = 'https://www.nycu.edu.tw/'
         super().__init__(url)
 
-    def action(self, driver):
-        driver.maximize_window()
-        driver.find_element(By.LINK_TEXT, '消息').click()
-        list_item = driver.find_element(By.ID, '-tab').find_element(
+    def maximize_window(self):
+        self.driver.maximize_window()
+
+    def click_nav_item(self):
+        self.driver.find_element(By.LINK_TEXT, '消息').click()
+
+    def click_first_new(self):
+        list_item = self.driver.find_element(By.ID, '-tab').find_element(
             By.TAG_NAME, 'li')
         list_item.find_element(By.TAG_NAME, 'a').click()
+
+        title = self.driver.find_element(By.CLASS_NAME, 'entry-title').text
+        content = self.driver.find_element(By.CLASS_NAME, 'entry-content').text
+
+        print(f'{title}\n{content}')
+
+    def action(self):
+        self.maximize_window()
+        self.click_nav_item()
+        self.click_first_new()
 
 
 class GoogleTestStep(WebTestStep):
@@ -38,7 +53,7 @@ class GoogleTestStep(WebTestStep):
         url = 'https://www.google.com'
         super().__init__(url)
 
-    def action(self, driver):
+    def action(self):
         pass
 
 
@@ -54,9 +69,10 @@ class WebTest:
 
     def load_test_step(self, test_step):
         self.test_step = test_step
+        test_step.driver = self.driver
 
     def do_navigate(self):
-        self.test_step.navigate(self.driver)
+        self.test_step.navigate()
 
     def do_action(self):
-        self.test_step.action(self.driver)
+        self.test_step.action()
